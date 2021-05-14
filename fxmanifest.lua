c197d11eaf15910dd2b58238e0c0dc9c2e53dfa7
mirrox1337 <anton.per.larsson@gmail.com>
2021-05-14 19:46:02 +0200

    upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Drive V'
+version '5.0'
+
+files {
+    'handling.meta',
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'materials.dat',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'MATERIALS_FILE' 'materials.dat'
